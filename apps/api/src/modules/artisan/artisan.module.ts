2a7615733d5543b5ed738024089eba7d4b344d2c
Moussa <188249787+VEYNOM007@users.noreply.github.com>
2026-08-03 10:09:41 +0200

    feat(api): retourne un resetToken a la creation d'un artisan

--- apps/api/src/modules/artisan/artisan.module.ts
+++ apps/api/src/modules/artisan/artisan.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ArtisanService } from './artisan.service';
 import { ArtisanController } from './artisan.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [ArtisanService],
   controllers: [ArtisanController],
   exports: [ArtisanService],
